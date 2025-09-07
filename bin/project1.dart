bf25bc788feec8bfd1f5fb8bfec6047127730253
Fallujahrama <fallujahrc@gmail.com>
2025-09-07 20:26:18 +0700

    constanta dan final

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,5 +1,6 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = 'Fallujah';
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +8,7 @@ void main(List<String> arguments) {
     multi line comment
     banyak line komen
   */
+  // constanta dan final
+  final nama2 = 'Ramadi';
+  print("Nama saya $nama $nama2");
 }
