2f9c0df880ab1077003108d24da37600e946b1b2
Fallujahrama <fallujahrc@gmail.com>
2025-09-07 20:50:53 +0700

    variable tipe data

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 const String nama = 'Fallujah';
@@ -8,7 +10,23 @@ void main(List<String> arguments) {
     multi line comment
     banyak line komen
   */
+
   // constanta dan final
   final nama2 = 'Ramadi';
   print("Nama saya $nama $nama2");
+
+  // variabel
+  int umur = 21;
+  double tinggi = 165.5;
+  bool jenis_kelamin = true; // true = laki-laki, false = perempuan
+  String alamat = 'Jalan Semanggi Barat no 20';
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi meter");
+  stdout.writeln("Jenis kelamin saya ${jenis_kelamin ? 'Laki-laki' : 'Perempuan'}");
+  // if (jenis_kelamin==true) {
+  //   stdout.writeln("Jenis kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
